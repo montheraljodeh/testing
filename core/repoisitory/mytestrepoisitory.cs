789a585d61ec813277205d98d2c13693215150fd
Munther Al Joudeh <M.AlJoudeh.ext@Tahaluf.ae>
2022-07-31 16:27:06 +0300

    added

---
+++ core/repoisitory/mytestrepoisitory.cs
@@ -0,0 +1,16 @@
+﻿using core.data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace core.repoisitory
+{
+    public interface mytestrepoisitory
+    {
+
+
+        public bool insertmytest(mytest test);
+
+        public bool updatemytest(mytest test);
+    }
+}
